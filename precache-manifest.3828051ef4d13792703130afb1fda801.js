330b7cb8325ad7dba8899918e1b41e4609ff50b8
yellowful <richenyou@gmail.com>
2020-04-28 16:49:11 +0800

    Updates

---
+++ precache-manifest.3828051ef4d13792703130afb1fda801.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739a77ea1620a407be72ca47d3ebaaaf",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "4b4b98d72d00b0038401",
+    "url": "/smartbrain/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "3d1e27a686aad96310a2",
+    "url": "/smartbrain/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4b4b98d72d00b0038401",
+    "url": "/smartbrain/static/js/2.4973e50a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.4973e50a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1e27a686aad96310a2",
+    "url": "/smartbrain/static/js/main.fae4f6c0.chunk.js"
+  },
+  {
+    "revision": "be86346f47316a9fda85",
+    "url": "/smartbrain/static/js/runtime-main.bb56a74b.js"
+  }
+]);
