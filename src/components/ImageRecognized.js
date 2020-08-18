b67f2f2488426bc184f82dbea1c2387e85e6ee83
yellowfully <richenyou@gmail.com>
2020-08-18 12:19:36 +0800

    bugs of image distorsion after clarifai fixed

--- src/components/ImageRecognized.js
+++ src/components/ImageRecognized.js
@@ -5,30 +5,28 @@ const ImageRecognized = ({appImageURL,answer,faceBox}) => {
 
     if (answer!==''){
         return(
-            <div className="mt1 tc flex flex-column justify-center">
+            <div className="mt1 flex flex-column">
             {/* margin top,text center */}
-                <div>
-                    <p>The celebrity is <span className="f3 dark-blue">{answer}</span>.</p>
-                </div>
-                <div className="outer-box">
-                    <div className="image-box mh2 mb2">
-                        <img id="celebrity-pic" alt="celebrity" src={appImageURL} />
-                        <div className="bounding-box" style={{
-                            top:faceBox.top_row, 
-                            right:faceBox.right_col, 
-                            left:faceBox.left_col, 
-                            bottom:faceBox.bottom_row}}>
+                    <p className="tc">The celebrity is <span className="f3 dark-blue">{answer}</span>.</p>
+                <div className="mt2 flex justify-center">
+                        <div className="image-box mh2">
+                            <img id="celebrity-pic" alt="celebrity" src={appImageURL} />
+                            <div className="bounding-box" style={{
+                                top:faceBox.top_row, 
+                                right:faceBox.right_col, 
+                                left:faceBox.left_col, 
+                                bottom:faceBox.bottom_row}}>
+                            </div>
                         </div>
-                    </div>
                 </div>
             </div>
         )
     } else {
         return(
             <div className="mt2 flex justify-center">
-                <div className="image-box mh2 mb2">
-                    <img id="celebrity-pic" alt="celebrity" src={appImageURL} />
-                </div>
+                    <div className="image-box mh2">
+                        <img id="celebrity-pic" alt="celebrity" src={appImageURL} />
+                    </div>
             </div>
         )
     }
