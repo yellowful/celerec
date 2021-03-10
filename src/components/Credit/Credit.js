0ae472475bc65ae202b48fcae9eddf8a728ebf3c
yellowfully <richenyou@gmail.com>
2021-03-10 18:06:15 +0800

    change the links of the Credit component

--- src/components/Credit/Credit.js
+++ src/components/Credit/Credit.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import {FormattedMessage} from 'react-intl'
+import { FormattedMessage } from 'react-intl'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import './Credit.css';
 
 //作者資訊
 const Credit = () => {
-    return(
-        <div>
-            <FormattedMessage
-                id='credit'
-                defaultMessage="App build by <code>Richard Huang</code>"
-                values={{code:(text)=><a  href="https://www.bdr.rocks/about/#contact" title="蟲探理查">{text}</a>}}
-            />
-        </div>
+    return (
+        <footer>
+            <span>
+                <FormattedMessage
+                    id='credit'
+                    defaultMessage="App build by <code>Richard Huang</code>"
+                    values={{ code: (text) => <a href="https://www.bdr.rocks/about/#contact" title="蟲探理查">{text}</a> }}
+                />
+            </span>
+            <span className="ml2">
+                <a href="https://github.com/yellowful/celerec" title="蟲探理查" target="_blank" rel="noopener">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+            </span>
+
+        </footer>
     )
 }
 
