1e84f374d829672b97375ce8244ab63c4d57f3ac
yellowfully <richenyou@gmail.com>
2021-03-22 11:56:20 +0800

    mark font awesomeicon in searchbar component because of unused

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import ImageUpload from './ImageUpload/ImageUpload.js';
 
