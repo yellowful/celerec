9a3bd9c7d71d48be809a1efffb2cac99baeef827
yellowfully <richenyou@gmail.com>
2021-08-10 11:31:53 +0800

    add translation to InvalidInput component

--- src/components/InvalidInput/InvalidInput.js
+++ src/components/InvalidInput/InvalidInput.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { FormattedMessage } from 'react-intl'
 
 // 要給FormSubmit用的
 // 當有錯誤訊息時要顯示錯誤訊息
 const InvalidInput = ({loginError}) => {
     if(loginError===true){
         return(
-            <p className="red">Invalid login or register! </p>
+            <p className="red">
+                <FormattedMessage id='invalid' />
+            </p>
         )
     } else {
         return false
